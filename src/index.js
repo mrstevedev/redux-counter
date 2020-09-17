7be45a7a18d9e27eaff16aa31912dc2a22c39e3b
Steven Polido <steven.polido@vistage.com>
2020-09-17 10:08:57 -0700

    Removed store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, combineReducers } from "redux";
-import allReducers from "./reducers";
+import store from './store';
 import { Provider } from "react-redux";
-
-const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <Provider store={ store }>
