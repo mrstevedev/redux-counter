ead3bc691055c9dfc49e6e4c958150eec9271ced
Steven Polido <steven.polido@vistage.com>
2020-09-17 13:21:52 -0700

    Added a todos item to combineReducers object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 // Import all reducers
 import counterReducer from './counter';
 import loggedReducer from './isLogged';
+import todos from './todos';
 import { combineReducers } from 'redux'; 
 
 const allReducers = combineReducers({
+    todos: todos,
     counter: counterReducer,
-    isLogged: loggedReducer
+    isLogged: loggedReducer,
 });
 
 export default allReducers;
