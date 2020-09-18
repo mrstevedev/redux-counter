a51e1eefb2455feb019fc18c0366baf4101582ea
Steven Polido <steven.polido@vistage.com>
2020-09-18 12:43:01 -0700

    Added class completed to list item to toggle completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,13 @@ import { useDispatch } from "react-redux";
 import { toggleTodo } from "../actions";
 
 function Todo({ id, todo, completed }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   return (
-    <li id={id} onClick={() => dispatch(toggleTodo(id))}>
+    <li
+      id={id}
+      onClick={() => dispatch(toggleTodo(id))}
+      className={completed ? "completed" : null}
+    >
       <p>{todo}</p>
     </li>
   );
