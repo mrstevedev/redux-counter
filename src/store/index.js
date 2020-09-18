b04b3fdd4676e4366d097d9df5b5a89aabfad7b2
Steven Polido <steven.polido@vistage.com>
2020-09-17 17:31:52 -0700

    Removed mock data

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,11 @@
 import { createStore } from "redux";
 import allReducers from "../reducers";
 
-const persistedState = {
-    todos: [{
-        id: 0,
-        todo: 'Learn Redux',
-        completed: false
-    }]
-}
-
 const store = createStore(
   allReducers,
-  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-console.log(store.getState())
+console.log(store.getState());
 
 export default store;
