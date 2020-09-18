a1e1b6ee77d39f7894500fc1a0f294d713206651
Steven Polido <steven.polido@vistage.com>
2020-09-18 12:42:10 -0700

    Added TOGGLE_TODO reducer logic to update completed in state

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -12,13 +12,9 @@ const todos = (state = [], action) => {
         }        
       ];
     case TOGGLE_TODO:
-        if(action.payload) {
-            console.log(action.payload)
-        }
-        return [
-            // ...state, { id: action.id, completed: true }
-            ...state
-        ]
+        return state.map(todo => 
+            todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo 
+        )        
     default:
       return state;
   }
